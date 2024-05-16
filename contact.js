e80688fa079a50d046f52715896e536bda4e10e5
emmanmallari <emmanuel.carlos.mallari@adamson.edu.ph>
2024-05-16 09:20:59 +0800

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,7 +1,4 @@
-/* scripts.js */
-
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for your message! We will get back to you soon.');
-    // You can add additional code here to handle form submission, such as sending the data to a server.
 });
